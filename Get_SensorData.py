1e8d3e72ce8685d758fdfbedc1ae92a441d7c670
ccmylady <warriorsam@163.com>
2022-10-02 09:07:00 +0800

    20221022，临时传输。

--- Get_SensorData.py
+++ Get_SensorData.py
@@ -5,46 +5,29 @@
  modified 220903 by XPC
 """
 
-from __future__ import print_function
-
 import modbus_tk
 import modbus_tk.defines as cst
-from modbus_tk import modbus_tcp, hooks
+from modbus_tk import modbus_tcp
 import logging
+logging.basicConfig(level=logging.DEBUG,
+                    format="%(asctime)s - %(levelname)s - %(threadName)s - %(message)s"
+                    ,datefmt="%Y-%m-%d %H:%M:%S"
+                    )
 
 def main():
     """main"""
-    logger = modbus_tk.utils.create_logger("console", level=logging.DEBUG)
-
-    def on_after_recv(data):
-        master, bytes_data = data
-        logger.info(bytes_data)
-
-    hooks.install_hook('modbus.Master.after_recv', on_after_recv)
-
+    datas_read=[]
+    alarm=""
     try:
-
-        def on_before_connect(args):
-            master = args[0]
-            logger.debug("on_before_connect {0} {1}".format(master._host, master._port))
-
-        hooks.install_hook("modbus_tcp.TcpMaster.before_connect", on_before_connect)
-
-        def on_after_recv(args):
-            response = args[1]
-            logger.debug("on_after_recv {0} bytes received".format(len(response)))
-
-        hooks.install_hook("modbus_tcp.TcpMaster.after_recv", on_after_recv)
-
         # Connect to the slave
         master = modbus_tcp.TcpMaster()
         master.set_timeout(5.0)
-        logger.info("connected")
+        logging.info("connected")
 
         # rerutn sensordata modified 220903
-        data_read=master.execute(1, cst.READ_HOLDING_REGISTERS, 0, 10)
+        datas_read=master.execute(1, cst.READ_HOLDING_REGISTERS, 0, 10)
 
-        logger.info(data_read)
+        logging.info(datas_read)
 
 
         # logger.info(master.execute(1, cst.READ_HOLDING_REGISTERS, 0, 2, data_format='f'))
@@ -64,7 +47,9 @@ def main():
         # logger.info(master.execute(1, cst.WRITE_MULTIPLE_REGISTERS, 100, output_value=xrange(12)))
 
     except modbus_tk.modbus.ModbusError as exc:
-        logger.error("%s- Code=%d", exc, exc.get_exception_code())
+        logging.info("%s- Code=%d", exc, exc.get_exception_code())
+
+    return datas_read
 
 if __name__ == "__main__":
     main()
--- main.py
+++ main.py
@@ -6,18 +6,32 @@
 
 import logging
 logging.basicConfig(level=logging.DEBUG,
-                    format="%(asctime)s - %(levelname)s - %(message)s"
+                    format="%(asctime)s - %(levelname)s - %(threadName)s - %(message)s"
                     ,datefmt="%Y-%m-%d %H:%M:%S"
                     )
 import threading
+import time
 
-from Modbus_TCP_master import main as read_sensor_data
+from Get_SensorData import main as get_sensor_data
 
+
+class RepeatingTimer(threading.Timer):
+    def run(self):
+        while not self.finished.is_set():
+            self.function(*self.args, **self.kwargs)
+            self.finished.wait(self.interval)
 
 def main():
-    # TODO 线程1读取传感器数据
-    threadObj=threading.Thread(target=read_sensor_data)
-    threadObj.start()
+    flag_getSensorData=True
+    # TODO 定时循环读取读取传感器数据
+    Thread1 = RepeatingTimer(10.0, get_sensor_data)
+    Thread1.start()
+
+
+    # Thread1.cancel()
+
+
+
     # TODO 将读取数据存入数据库(或写入CSV？)
     # TODO 线程2实时动态显示数据
     # TODO 生成报表
